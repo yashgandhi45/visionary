54352b3bc68f2dde90f83ecfbe2a2552e0d17144
Yash Gandhi <yashgandhi4505@gmail.com>
2025-02-23 20:35:43 +0530

    BoardList and updated Images, need to fix searchParams

--- app/(dashboard)/_components/emptyboards.tsx
+++ app/(dashboard)/_components/emptyboards.tsx
@@ -1,14 +1,28 @@
+"use client";
+import { useOrganization } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export const EmptyBoards = () => {
+    const {organization} = useOrganization();
+    const create = useMutation(api.board.create);
+    const onClick = () => {
+
+        if(!organization) return;
+        create ({
+            orgId : organization.id,
+            title : "Untitled"
+        });
+    };
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image src="/Home.jpg" width={140} height={140} alt="Empty"/>
             <h2 className="text-muted-foreground text-sm mt-2">Create your first board!</h2>
             <p>Start by creating a board for your organization</p>
             <div className="mt-6">
-                <Button size="lg">Create Board</Button>
+                <Button onClick={onClick} size="lg">Create Board</Button>
             </div>
         </div>
     )
