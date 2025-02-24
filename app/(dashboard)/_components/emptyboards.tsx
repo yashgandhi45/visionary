0ca29d3677567afadab42e2ce7fcbcfdb6f41dc5
Yash Gandhi <yashgandhi4505@gmail.com>
2025-02-24 10:53:34 +0530

    Added Custom Hook

--- app/(dashboard)/_components/emptyboards.tsx
+++ app/(dashboard)/_components/emptyboards.tsx
@@ -4,14 +4,15 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/useapimutation";
 
 export const EmptyBoards = () => {
     const {organization} = useOrganization();
-    const create = useMutation(api.board.create);
+    const {mutate, pending} = useApiMutation(api.board.create);
     const onClick = () => {
 
         if(!organization) return;
-        create ({
+        mutate ({
             orgId : organization.id,
             title : "Untitled"
         });
@@ -22,7 +23,7 @@ export const EmptyBoards = () => {
             <h2 className="text-muted-foreground text-sm mt-2">Create your first board!</h2>
             <p>Start by creating a board for your organization</p>
             <div className="mt-6">
-                <Button onClick={onClick} size="lg">Create Board</Button>
+                <Button disabled={pending} onClick={onClick} size="lg">Create Board</Button>
             </div>
         </div>
     )
--- app/(dashboard)/_components/emptysearch.tsx
+++ app/(dashboard)/_components/emptysearch.tsx
@@ -4,7 +4,7 @@ export const EmptySearch = () => {
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image src="/placeholders/4.svg" width={140} height={140} alt="Empty"/>
-            <h2 className="text-muted-foreground text-sm mt-2">No Elements Found!</h2>
+            <h2 className="text-muted-foreground text-sm mt-2">No results found!</h2>
             <p>Try searching for something else</p>
         </div>
     )
