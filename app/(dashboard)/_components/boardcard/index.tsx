48e834c494f0942cba787960c6eeed2a7eb85f50
Yash Gandhi <yashgandhi4505@gmail.com>
2025-02-24 19:51:01 +0530

    Completed Board Action Delete and copy link

--- app/(dashboard)/_components/boardcard/index.tsx
+++ app/(dashboard)/_components/boardcard/index.tsx
@@ -6,6 +6,8 @@ import { formatDistanceToNow } from "date-fns";
 import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 interface BoardCardProps {
     id: string;
     title: string;
@@ -30,6 +32,11 @@ export const BoardCard = ({id, title, authorId, authorName, createdAt, imageUrl,
                 <div className="relative flex-1 bg-amber-50">
                     <Image src={imageUrl} alt = {title} fill className="object-fit"/>
                     <Overlay/>
+                    <Actions id={id} title={title} side="right">
+                        <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+                            <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity"/>
+                        </button>
+                    </Actions>
                 </div>
                 <Footer isFavorite = {isFavorite} title={title} authorLabel={authorLabel} createdAtLabel={createdAtLabel}
                  onClick={()=>{}} disabled={false}/>
