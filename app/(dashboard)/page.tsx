ee49302d4c187e2fab28b48d14ca81d988392cf5
Yash Gandhi <yashgandhi4505@gmail.com>
2025-03-04 13:02:49 +0530

    Fixed page.tsx issue

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,26 +1,25 @@
-"use client";;
+"use client";
+
 import { use } from "react";
 import { BoardList } from "./_components/boardlist";
 import { EmptyOrg } from "./_components/emptyorg";
 import { useOrganization } from "@clerk/nextjs";
 
 interface DashBoardPageProps {
-    searchParams : Promise<{
-        search?:string;
-        favorites?:string;
-    }>
+  searchParams: Promise<{
+    search?: string;
+    favorites?: string;
+  }>;
 }
 
-export const DashBoardPage = (props: DashBoardPageProps) => {
-    const searchParams = use(props.searchParams);
-    const {organization} = useOrganization();
-    return (
-        <div className="flex-1 h-[calc(100%-80px)] p-6">
-            {/* {JSON.stringify(searchParams)} */}
-            {!organization ? <EmptyOrg/> : (
-                <BoardList orgId = {organization.id} query={searchParams}/>
-            )}
-        </div>
-    )
-}
+const DashBoardPage = (props: DashBoardPageProps) => {
+  const searchParams = use(props.searchParams);
+  const { organization } = useOrganization();
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrg /> : <BoardList orgId={organization.id} query={searchParams} />}
+    </div>
+  );
+};
+
 export default DashBoardPage;
